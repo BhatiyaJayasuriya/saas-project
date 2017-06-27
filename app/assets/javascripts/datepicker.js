ad4daf42d75ebdc3f7267b61c55d7c7e4644d55f
Bhatiya Jayasuriya <bhatiyajayasuriya@gmail.com>
2017-06-27 20:32:50 +0000

    create projects

---
+++ app/assets/javascripts/datepicker.js
@@ -0,0 +1,17 @@
+/* global $ */
+
+$(document).ready(function() {
+  $(function() {
+    $('.datepicker').datepicker({format: 'yyyy-mm-dd'});
+  });
+});
+
+// var date_picker;
+
+// date_picker = function() {
+  
+// };
+
+// $(document).ready(function() {
+  
+// });
